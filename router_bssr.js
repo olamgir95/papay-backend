c1e19cf5bfe9ada96b91a1f743ed034e38e67153
Olamgir <ollomurodovolamgir233@gmail.com>
2023-10-23 23:00:26 +0900

    feat: modify image uploader utils

--- router_bssr.js
+++ router_bssr.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router_bssr = express.Router();
 const restaurantController = require("./controllers/restaurantController");
 const productController = require("./controllers/productController");
-const { uploadProductImage } = require("./utils/upload-multer");
+const upload_product = require("./utils/upload-multer")("products");
 
 /*************************/
 /*  BSSR EJS */
@@ -23,7 +23,7 @@ router_bssr.get("/products/menu", restaurantController.getMyRestaurantData);
 router_bssr.post(
   "/products/create",
   restaurantController.validateAuthRestaurant,
-  uploadProductImage.single("product_image"),
+  upload_product.array("product_images", 5),
   productController.addNewProduct
 );
 router_bssr.post("/products/edit/:id", productController.updateChosenProduct);
--- utils/upload-multer.js
+++ utils/upload-multer.js
@@ -2,16 +2,23 @@ const path = require("path");
 const multer = require("multer");
 const uuid = require("uuid");
 
-const product_storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads/products");
-  },
-  filename: function (req, file, cb) {
-    console.log(file);
-    const extension = path.parse(file.originalname).ext;
-    const random_name = uuid.v4() + extension;
-    cb(null, random_name);
-  },
-});
+/* Multer image uploader*/
+function getTargetImageStorage(address) {
+  return multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./uploads/products");
+    },
+    filename: function (req, file, cb) {
+      console.log(file);
+      const extension = path.parse(file.originalname).ext;
+      const random_name = uuid.v4() + extension;
+      cb(null, random_name);
+    },
+  });
+}
+const makeUploader = (address) => {
+  const storage = getTargetImageStorage();
+  return multer({ storage: storage });
+};
 
-module.exports.uploadProductImage = multer({ storage: product_storage });
+module.exports = makeUploader;
