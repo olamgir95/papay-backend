f110a597e380388b0def84a5d7c8e5b75f2b3770
Olamgir <ollomurodovolamgir233@gmail.com>
2023-10-26 19:39:39 +0900

    featr:new products make restaurant's users

--- models/Product.js
+++ models/Product.js
@@ -8,11 +8,21 @@ class Product {
     this.productModel = ProductModel;
   }
 
+  async getAllProductsDataResto(member) {
+    try {
+      member._id = shapeIntoMongooseObjectId(member._id);
+      const result = await this.productModel.find({
+        restaurant_mb_id: member._id,
+      });
+      assert.ok(result, Definer.general_err1);
+      console.log(result);
+    } catch {}
+  }
   async addNewProductData(data, member) {
     try {
       data.restaurant_mb_id = shapeIntoMongooseObjectId(member._id);
 
-      const new_product = new this.ProductModel(data);
+      const new_product = new this.productModel(data);
       const result = await new_product.save();
 
       assert.ok(result, Definer.product_err1);
