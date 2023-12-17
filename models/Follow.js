67548aaee5ae2d1ddf3559805ac3cc7af6d91c7a
Olamgir <ollomurodovolamgir233@gmail.com>
2023-12-17 12:05:17 +0900

    fix: modify getChosenMember api ny  folliwing back data logic

--- models/Follow.js
+++ models/Follow.js
@@ -158,7 +158,7 @@ class Follow {
       ];
 
       if (member._id === inquiry.mb_id) {
-        aggregateQuery.push(lookup_auth_member_following(follow_id));
+        aggregateQuery.push(lookup_auth_member_following(follow_id, "follows"));
       }
 
       const result = await this.followModel.aggregate(aggregateQuery).exec();
--- models/Member.js
+++ models/Member.js
@@ -2,7 +2,10 @@ const assert = require("assert");
 const Definer = require("../lib/mistake");
 const MemberModel = require("../schema/member.model");
 const bcrypt = require("bcryptjs");
-const { shapeIntoMongooseObjectId } = require("../lib/config");
+const {
+  shapeIntoMongooseObjectId,
+  lookup_auth_member_following,
+} = require("../lib/config");
 const View = require("./View");
 
 class Member {
@@ -47,20 +50,23 @@ class Member {
 
   async getChosenMemberData(member, id) {
     try {
+      const auth_mb_id = shapeIntoMongooseObjectId(member?._id);
       id = shapeIntoMongooseObjectId(id);
 
-      console.log("member:", member);
+      let aggregateQuery = [
+        { $match: { _id: id, mb_status: "ACTIVE" } },
+        { $unset: "mb_password" },
+      ];
+
       if (member) {
-        // condition if nmot seen before
         await this.viewChosenItemByMember(member, id, "member");
+        //todo: check auth member likiedf the chosen member
+        aggregateQuery.push(
+          lookup_auth_member_following(auth_mb_id, "members")
+        );
       }
-      const result = await this.memberModel
-        .aggregate([
-          { $match: { _id: id, mb_status: "ACTIVE" } },
-          { $unset: "mb_password" },
-          //todo: check auth member likiedf the chosen member
-        ])
-        .exec();
+
+      const result = await this.memberModel.aggregate(aggregateQuery).exec();
       assert.ok(result, Definer.general_err2);
       return result[0];
     } catch (err) {
